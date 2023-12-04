f262421d37aad077dcb6b6cd9e824da9f02cc993
Kuba <kubator21@protonmail.com>
2023-12-03 20:02:50 -0600

    active tabs in job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,6 +20,8 @@ import {
 
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
+
+const tabs = ["About", "Qualifications", "Responsibilities"];
 const JobDetails = () => {
   const params = useGlobalSearchParams();
   const router = useRouter();
@@ -29,6 +31,7 @@ const JobDetails = () => {
   });
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {};
   return (
@@ -72,7 +75,11 @@ const JobDetails = () => {
                 companyName={data[0].employer_name}
                 location={data[0].job_country}
               />
-              <JobTabs />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
             </View>
           )}
         </ScrollView>
